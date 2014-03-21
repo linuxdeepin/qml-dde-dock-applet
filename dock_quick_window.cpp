3bde02736109d066c212b620397fcdaf230aae64
snyh <snyh@snyh.org>
2014-03-21 12:21:00 +0800

    implement dock.dde.Entry

--- dock_quick_window.cpp
+++ dock_quick_window.cpp
@@ -1,5 +1,6 @@
 #include "dock_quick_window.h"
 #include <QDBusConnection>
+#include <QDBusMetaType>
 
 DockQuickWindow::DockQuickWindow(QQuickWindow *parent):
     QQuickWindow(parent),
@@ -18,20 +19,43 @@ DockQuickWindow::~DockQuickWindow()
     delete this->applet;
 }
 
+void DockQuickWindow::setMenu(const QString& m)
+{
+    m_menu = m;
+    Q_EMIT menuChanged(m);
+    applet->setData("menu", m);
+}
+void DockQuickWindow::setIcon(const QString& v)
+{
+    m_icon = v;
+    Q_EMIT iconChanged(v);
+    applet->setData("icon", v);
+}
+void DockQuickWindow::setTitle(const QString& v)
+{
+    QQuickWindow::setTitle(v);
+    m_title= v;
+    Q_EMIT titleChanged(v);
+    applet->setData("title", v);
+}
+void DockQuickWindow::setStatus(const qint32 v)
+{
+    m_status = v;
+    Q_EMIT statusChanged(v);
+    applet->setData("status", QString::number(v));
+}
+
 
 
 DockApplet::DockApplet(DockQuickWindow* parent, int xid) :
     QDBusAbstractAdaptor(parent),
     m_parent(parent)
 {
+    qDBusRegisterMetaType<StringMap>();
     QString id  = QString::number(xid);
     QDBusConnection::sessionBus().registerService("dde.dock.entry.Applet" + id);
-    qDebug() << "Register:" << QDBusConnection::sessionBus().registerObject("/dde/dock/entry/Applet" + id, parent);
+    qDebug() << "Register:" << QDBusConnection::sessionBus().registerObject("/dde/dock/entry/v1/Applet" + id, parent);
 
-    QObject::connect(m_parent, SIGNAL(iconChanged(QString)), this, SIGNAL(iconChanged(QString)));
-    QObject::connect(m_parent, SIGNAL(menuChanged(QString)), this, SIGNAL(menuChanged(QString)));
-    QObject::connect(m_parent, SIGNAL(titleChanged(QString)), this, SIGNAL(titleChanged(QString)));
-    QObject::connect(m_parent, SIGNAL(statusChanged(qint32)), this, SIGNAL(statusChanged(qint32)));
-
+    setData("xid", id);
     qDebug() << "CreateProxyer for:" <<  xid;
 }
--- dock_quick_window.h
+++ dock_quick_window.h
@@ -3,7 +3,7 @@
 
 #include <QQuickItem>
 #include <QQuickWindow>
-#include <QtDBus/QDBusAbstractAdaptor>
+#include <QDBusAbstractAdaptor>
 #include <QRect>
 
 class DockApplet;
@@ -22,24 +22,25 @@ public:
     DockQuickWindow(QQuickWindow *parent = 0);
     ~DockQuickWindow();
 
-    const QString& menu() { return m_menu; }
-    void setMenu(const QString& m) { m_menu = m; Q_EMIT menuChanged(m);}
-    Q_SIGNAL void menuChanged(QString);
-
     const QString& id() { return m_id; }
     void setId(const QString& v) { m_id = v; Q_EMIT idChanged(v);}
     Q_SIGNAL void idChanged(QString);
 
+
+    const QString& menu() { return m_menu; }
+    void setMenu(const QString& m);
+    Q_SIGNAL void menuChanged(QString);
+
     const QString& icon() {return m_icon; }
-    void setIcon(const QString& v) { m_icon = v; Q_EMIT iconChanged(v);}
+    void setIcon(const QString& v);
     Q_SIGNAL void iconChanged(QString);
 
     const QString& title() {return m_title; }
-    void setTitle(const QString& v) { QQuickWindow::setTitle(v); m_title= v; Q_EMIT titleChanged(v);}
+    void setTitle(const QString& v);
     Q_SIGNAL void titleChanged(QString);
 
     qint32 status() {return m_status; }
-    void setStatus(const qint32 v) { m_status = v; Q_EMIT statusChanged(v);}
+    void setStatus(const qint32 v);
     Q_SIGNAL void statusChanged(qint32);
 
 private:
@@ -52,37 +53,35 @@ private:
     DockApplet* applet;
 };
 
+typedef QMap<QString,QString> StringMap;
+Q_DECLARE_METATYPE(StringMap)
+
 class DockApplet : public QDBusAbstractAdaptor {
     Q_OBJECT
     Q_CLASSINFO("D-Bus Interface", "dde.dock.Entry")
-    Q_PROPERTY(QString Type READ type)
-    Q_PROPERTY(QString Id READ id)
-    Q_PROPERTY(quint32 XID READ xid)
 
-    Q_PROPERTY(QString Menu READ menu NOTIFY menuChanged)
-    Q_PROPERTY(QString Title READ title NOTIFY titleChanged)
-    Q_PROPERTY(QString Icon READ icon NOTIFY iconChanged)
-    Q_PROPERTY(qint32 Status READ status NOTIFY statusChanged)
+    Q_PROPERTY(QString Id READ id)
+    Q_PROPERTY(QString Type READ type)
+    Q_PROPERTY(StringMap Data READ data)
 
 public:
     DockApplet(DockQuickWindow* parent, int xid);
 
+    const QMap<QString,QString>& data() {
+	return m_data;
+    }
     const QString type() { return "Applet"; }
     const QString id() {return m_parent->id(); }
-    const QString menu() { return m_parent->menu(); }
-    const QString title() { return m_parent->title(); }
-    quint32 xid() { return m_parent->winId(); }
-    const QString icon() { return m_parent->icon(); }
-    qint32 status() { return m_parent->status(); }
-    bool viewable() { return m_parent->isVisible(); }
-    QRect allocation() { return QRect(QPoint(m_parent->x(), m_parent->y()), m_parent->size()); }
 
-    Q_SIGNAL void menuChanged(QString);
-    Q_SIGNAL void titleChanged(QString);
-    Q_SIGNAL void iconChanged(QString);
-    Q_SIGNAL void statusChanged(qint32);
+    void setData(const QString& k, const QString& v) {
+	m_data[k] = v;
+	Q_EMIT DataChanged(k,v);
+	qDebug() << "SetData" <<  k <<  m_data[k];
+    }
 
+    Q_SIGNAL void DataChanged(QString,QString);
 private:
+    StringMap  m_data;
     DockQuickWindow* m_parent;
 };
 
